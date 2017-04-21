d2b761183c2289c93201233c418453943e061faf
Glove An <GloveInTheHouse@gmail.com>
2017-04-21 09:26:02 +0800

    file re-arrangement

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -182,26 +182,12 @@ class MaxHeap(Heap):
         self._heap[i] = key
 
 
-# chapter 6.4
-def heap_sort(A, Heap=MaxHeap):
-    heap = Heap(A)
-    for i in reversed(range(1, heap._heap_size)):
-        heap._heap[0], heap._heap[i] = heap._heap[i], heap._heap[0]
-        heap._heap_size -= 1
-        heap._heapify(0)
-    return heap._heap
-
-
 # chapter 6.5
 def PriorityQueue(A, Heap=MaxHeap):
     return Heap(A)
 
 
 if __name__ == "__main__":
-    A = [5, 3, 17, 10, 84, 19, 6, 22, 9]
-    print(heap_sort(A))
-    print(heap_sort(A, MinHeap))
-
     A = [15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1]
     queue = PriorityQueue(A)
     for _ in range(len(A)):
