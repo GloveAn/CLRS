da58d46f4960a977eff047fc94b9f9ee2fadc12e
Glove An <GloveInTheHouse@gmail.com>
2017-05-23 16:49:16 +0800

    20170523-1649

--- graph/graph.py
+++ graph/graph.py
@@ -23,6 +23,7 @@ class Graph():
         raise NotImplementedError
 
 
+# chapter 22.1
 class MatrixGraph(Graph):
     def __init__(self, V, E):
         super().__init__(V, E)
@@ -45,6 +46,7 @@ class MatrixGraph(Graph):
         return r
 
 
+# chapter 22.1
 class AdjListGraph(Graph):
     def __init__(self, V, E):
         super().__init__(V, E)
@@ -98,6 +100,7 @@ def bfs(G, s):
         # u.color = BLACK
 
 
+# chapter 22.2
 def print_path(G, s, v):
     if v == s:
         print(s.id)
@@ -106,6 +109,71 @@ def print_path(G, s, v):
     else:
         print_path(G, s, v.pi)
         print(v.id)
+
+
+# chapter 22.3, exerceises 22.3-4
+# errata: if line 8 of DFS-VISIT was removed.
+def dfs(G):
+    def dfs_visit(G, u):
+        nonlocal time
+
+        time = time + 1
+        u.d = time
+        u.color = GRAY
+        for v in G.neighbors(u):
+            if v.color == WHITE:
+                v.pi = u
+                dfs_visit(G, v)
+        # u.color = BLACK
+        time = time + 1
+        u.f = time
+
+
+    WHITE = 0
+    GRAY = 1
+    # BLACK = 2
+
+    for u in G.vertices():
+        u.color = WHITE
+        u.pi = None
+    time = 0
+    for u in G.vertices():
+        if u.color == WHITE:
+            dfs_visit(G, u)
+
+
+# exerceises 22.3-7
+def dfs_stack(G):
+    def dfs_visit_stack(G, u):
+        stack = [u]
+        time = 0
+
+        while len(stack):
+            time = time + 1
+
+            u = stack.pop()
+            if u.color == WHITE:
+                u.color = GRAY
+                u.d = time
+                stack.append(u)
+                for v in G.neighbors(u):
+                    if v.color == WHITE:
+                        stack.append(v)
+            else:
+                u.f = time
+
+
+    WHITE = 0
+    GRAY = 1
+
+    for u in G.vertices():
+        u.color = WHITE
+        u.pi = None
+    time = 0
+    for u in G.vertices():
+        if u.color == WHITE:
+            dfs_visit_stack(G, u)
+
 
 
 if __name__ == "__main__":
@@ -143,3 +211,24 @@ if __name__ == "__main__":
     G = AdjListGraph(V, E)
     bfs(G, V[1])
     print_path(G, V[1], V[3])
+
+    print()
+
+    V = {}
+    for i in range(8):
+        V[i] = Vertex(i)
+    E = []
+    E.append((0, 1, 1))
+    E.append((0, 3, 1))
+    E.append((1, 4, 1))
+    E.append((2, 4, 1))
+    E.append((2, 5, 1))
+    E.append((3, 1, 1))
+    E.append((4, 3, 1))
+    E.append((5, 5, 1))
+
+    G = MatrixGraph(V, E)
+    dfs(G)
+
+    G = AdjListGraph(V, E)
+    dfs_stack(G)
